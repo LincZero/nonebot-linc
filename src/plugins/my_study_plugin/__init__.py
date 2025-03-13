06567e304fe4c10c9aadc3499daccf88ebdce2ef
Linc <762699299@qq.com>
2025-03-13 15:03:56 +0800

    enhance: my_study_plugin

--- src/plugins/my_study_plugin/__init__.py
+++ src/plugins/my_study_plugin/__init__.py
@@ -25,7 +25,7 @@ from nonebot.permission import SUPERUSER    # 用于筛选用户
 
 # 信息响应器 - 命令型 (`/<command> args` 格式)
 weather = on_command(
-    "debug",                        # id、响应名
+    "debug1",                       # id、响应名
     aliases={"weather", "天气"},    # 其他响应名
     # rule=to_me(),                 # 需要私聊或艾特
     priority=10,                    # 优先级10 (越小越优先)
@@ -38,7 +38,7 @@ from nonebot.params import CommandArg       # 参数 - 依赖注入
 
 # 事件处理函数 (装饰器)
 @weather.handle()
-async def handle_function(args: Message = CommandArg()):
+async def _(args: Message = CommandArg()):
     # 事件响应器
     logger.info(f'masterMsg 消息队列: {args}')
     logger.info(f'  命令跟随的内容: {args.extract_plain_text()}')
@@ -58,15 +58,42 @@ from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent # 群消息
 m = on_message(priority=10, block=False, permission=SUPERUSER)
 
 @m.handle()
-async def repeater(bot: Bot, event: GroupMessageEvent):
-    logger.info(f'''
-        信息: {event.raw_message}
-        发送人id: {event.user_id}
-        发送时间: {event.time}
-        回复: {event.reply}
-        发送源id: {event.get_session_id()}
-        群id: {event.group_id}''')
-    if event.reply:
+async def _(bot: Bot, event: GroupMessageEvent):
+    if (event.raw_message == '/debug2'):
         logger.info(f'''
+            信息: {event.raw_message}
+            发送人id: {event.user_id}
+            发送时间: {event.time}
             回复: {event.reply}
-            信息: {event.reply.message}''')
+            发送源id: {event.get_session_id()}
+            群id: {event.group_id}''')
+        if event.reply:
+            logger.info(f'''
+                回复: {event.reply}
+                信息: {event.reply.message}''')
+
+# ------------------------ ALC型 --------------------------
+
+from nonebot import require
+require("nonebot_plugin_alconna")
+
+from nonebot.rule import to_me
+from arclet.alconna import Alconna, Args
+from nonebot_plugin_alconna import Match, on_alconna
+
+weather = on_alconna( # [!code]
+    Alconna("/debug3", Args["location?", str]),
+    aliases={"/weather3", "/天气预报3"},
+    # rule=to_me(),
+)
+
+@weather.handle()
+async def handle_function(location: Match[str]):
+    if location.available:
+        weather.set_path_arg("location", location.result)
+
+@weather.got_path("location", prompt="请输入地名")
+async def got_location(location: str):
+    if location not in ["北京", "上海", "广州", "深圳"]:
+        await weather.reject(f"你想查询的城市 {location} 暂不支持，请重新输入！")
+    await weather.finish(f"今天{location}的天气是...")
